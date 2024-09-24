e956203e7c6b2404dda31de5314fa1265ba9eb1b
Rodrigo <101577451+SymphonyCat@users.noreply.github.com>
2024-09-24 13:35:37 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,56 @@
 import streamlit as st
-from llama_index import VectorStoreIndex, ServiceContext, Document
-from llama_index.llms import OpenAI
-import openai
-from llama_index import SimpleDirectoryReader
+from langchain.chains.summarize.map_reduce_prompt import prompt_template
+from langchain_community.llms import Ollama
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+import time
 
-openai.api_key = ollama run llama3.1
-st.header("Chat with the Streamlit docs 💬 📚")
+llm = Ollama(model="llama3.1:8b")
 
-if "messages" not in st.session_state.keys(): # Initialize the chat message history
-    st.session_state.messages = [
-        {"role": "assistant", "content": "Ask me a question about Streamlit's open-source Python library!"}
-    ]
+def main():
+    st.title("CircuitSage-Asistente Técnico")
 
-@st.cache_resource(show_spinner=False)
-def load_data():
-    with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
-        reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
-        docs = reader.load_data()
-        service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
-        index = VectorStoreIndex.from_documents(docs, service_context=service_context)
-        return index
+    bot_name = "CircuitSage"
+    bot_description = f"""Eres un asistente virtual especializado en resolver problemas técnicos de laptops y computadoras de sobremesa solamente. Te llamas {bot_name}, respondes preguntas con respuestas detalladas. Además, debes preguntar al usuario acorde al contexto del chat, y también preguntar al usuario para obtener una respuesta más detallada, pero solamente te presentaras con un hola y preguntando al usuario que se le ofrece o cual es su problema, cualquier tema que no este relacionada con el hardware de las computadoras y laptops descartalo y hacelo saber al usuario de forma contundente."""
 
-index = load_data()
-chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
-if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    if "chat_history" not in st.session_state:
+        st.session_state["chat_history"] = []
 
-for message in st.session_state.messages: # Display the prior chat messages
-    with st.chat_message(message["role"]):
-        st.write(message["content"])
+    prompt_template = ChatPromptTemplate.from_messages(
+        [
+            ("system", bot_description),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{input}"),
+        ]
+    )
 
-# If last message is not from assistant, generate a new response
-if st.session_state.messages[-1]["role"] != "assistant":
-    with st.chat_message("assistant"):
-        with st.spinner("Thinking..."):
-            response = chat_engine.chat(prompt)
-            st.write(response.response)
-            message = {"role": "assistant", "content": response.response}
-            st.session_state.messages.append(message) # Add response to message history
+    chain = prompt_template | llm
 
+    user_input = st.text_input("Escribe tu pregunta:", key="user_input")
+
+    if st.button("Enviar"):
+        if user_input.lower() == "adios":
+            st.stop()
+        else:
+            # Mostrar un mensaje de "espera" mientras se procesa la respuesta
+            with st.spinner("Generando respuesta, por favor espera..."):
+                start_time = time.time()
+                response = chain.invoke({"input": user_input, "chat_history": st.session_state["chat_history"]})
+                elapsed_time = time.time() - start_time
+                if elapsed_time > 60:
+                    st.warning("La generación de la respuesta está tardando más de lo esperado.")
+
+            st.session_state["chat_history"].append(HumanMessage(content=user_input))
+            st.session_state["chat_history"].append(AIMessage(content=response))
+
+    chat_display = ""
+    for msg in st.session_state["chat_history"]:
+        if isinstance(msg, HumanMessage):
+            chat_display += f"🦧Yo: {msg.content}\n"
+        elif isinstance(msg, AIMessage):
+            chat_display += f"🔧{bot_name}: {msg.content}\n"
+
+    st.text_area("Chat", value=chat_display, height=400, key="chat_area")
+
+if __name__ == '__main__':
+    main()
