dda1a84c54bc487c8a19d6d80ae9ad8961eb88cd
Rodrigo <101577451+SymphonyCat@users.noreply.github.com>
2024-09-24 13:15:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def generate_response(user_input, chat_history):
         "input": user_input,
         "chat_history": chat_history
     }
-    response = requests.post(f"{SERVER_URL}/api/chat", json=payload)
+    response = requests.post(f"{http://127.0.0.1:4040 }/api/chat", json=payload)
     if response.status_code == 200:
         return response.json().get("response", "No se recibió respuesta.")
     else:
