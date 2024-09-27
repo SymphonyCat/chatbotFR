86bda09523e7e6af37b5ca1f22ddea6c6ebbfeab
Rodrigo <101577451+SymphonyCat@users.noreply.github.com>
2024-09-27 14:27:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 import time
 
-llm = Ollama(model="llama3:8b")
+llm = Ollama(model="llama3:8b" server = "http://127.0.0.1")
 
 def main():
     st.title("CircuitSage-Asistente Técnico")
